c355cd07033480287386c3c27d9ce197ab575fe7
Christian Alamillo <calam002@ucr.edu>
2021-09-09 23:46:40 -0700

    Added defer attribute to src tag to ensure page has finished parsing before executing javascript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log('Hello there');
+
+
+function component() {
+    const header = document.createElement('div');
+    const homeButton = document.createElement('button');
+    const menuButton = document.createElement('button');
+    const contactInfoButton = document.createElement('button');
+
+    homeButton.innerText = 'Home';
+    menuButton.innerText = 'Menu';
+    contactInfoButton.innerText = 'Contact us!'
+
+    header.appendChild(homeButton);
+    header.appendChild(menuButton);
+    header.appendChild(contactInfoButton);
+
+    return header;
+}
+
+document.body.appendChild(component());
