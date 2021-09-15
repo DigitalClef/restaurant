35e9974566a5051613f9e08bb8bf413d12db710c
Christian Alamillo <calam002@ucr.edu>
2021-09-14 20:27:40 -0700

    Created js files

---
+++ src/menu.js
@@ -0,0 +1,30 @@
+const menu = function() {
+    const content = document.querySelector('#content');
+    const menuPage = document.createElement('div');
+
+    const header = document.createElement('h2');
+    header.innerText = 'Take a look at our menu!';
+
+    menuPage.appendChild(header);
+
+    menuPage.appendChild(menuItemGenerator('Chocolate Roll', 'Delicious and rich chocolate coating with sweet rice and sweet filling'));
+
+    content.appendChild(menuPage);
+}
+
+function menuItemGenerator(name, description) {
+    const menuItem = document.createElement('div');
+    menuItem.classList.add
+    const foodPic = document.createElement('img');
+    const foodDescription = document.createElement('p');
+    foodPic.setAttribute('src', `images/${name.replace(/\s+/g, '').toLowerCase()}.jpg`);
+    foodPic.setAttribute('alt', `${name}`);
+    foodDescription.innerText = description;
+
+    menuItem.appendChild(foodPic);
+    menuItem.appendChild(foodDescription);
+
+    return menuItem;
+}
+
+export {menu}
