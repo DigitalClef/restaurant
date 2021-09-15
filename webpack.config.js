7cf60c0503c0e3f17d9deca3585042d0d4a54f62
Christian Alamillo <calam002@ucr.edu>
2021-09-14 20:26:42 -0700

    Added module rule to implement image file utilization

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,14 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ]
+  }
+
 };
