17c624a6d5ff5d6a7b2c3e61d5b0aeeaf75fc114
Christian Alamillo <calam002@ucr.edu>
2021-09-14 20:28:57 -0700

    Imported menu.js module

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import {mainHeader} from './initial.js'
 import {home} from './home.js'
+import {menu} from './menu.js'
+
 
 mainHeader();
 document.querySelector('#home').onclick = function() {
     home();
 }
 
+menu();
