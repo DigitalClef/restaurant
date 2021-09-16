9ead3aad19c4dbd6ac3316cd45ceb0c59312d497
Christian Alamillo <calam002@ucr.edu>
2021-09-15 20:47:58 -0700

    Implemented logic to clear every node of content div beyond the first child when button is clicked

--- src/index.js
+++ src/index.js
@@ -5,13 +5,24 @@ import {contact} from './contact.js'
 import './style.css'
 
 mainHeader();
+home();
+
+function removeAllChildNodes() {
+    const content = document.querySelector('#content');
+    while (content.childElementCount !== 1) {
+        content.removeChild(content.lastChild);
+    }
+}
 
 document.querySelector('#home').onclick = function() {
+    removeAllChildNodes();
     home();
 }
 document.querySelector('#menu').onclick = function() {
+    removeAllChildNodes();
     menu();
 }
 document.querySelector('#contact').onclick = function() {
+    removeAllChildNodes();
     contact();
 }
